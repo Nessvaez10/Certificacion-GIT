7007cb82210669f4e5e651a78be00fbad6ee8235
Nessvaez10 <97480468+Nessvaez10@users.noreply.github.com>
2024-08-29 18:33:26 -0600

    normatividad
    ..

--- src/components/Normatividad.js
+++ src/components/Normatividad.js
@@ -10,12 +10,190 @@ function Normatividad() {
         <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/CPEUM.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
         Constitución Política de los Estados Unidos Mexicanos
         </a>
-        <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/CPEUM.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
-        Constitución Política de los Estados Unidos Mexicanos
+
+        <a href="http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Constitucion%20Politica%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+        Constitución Política del Estado de Hidalgo	
         </a>
-        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="list-group-item">
-          Ley ****
+        <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/LGTAIP.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+        Ley General de Transparencia y Acceso a la Información Pública
         </a>
+        <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/LGA.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+        Ley General de Archivos
+        </a>
+        <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/LGPDPPSO.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+        Ley General de protección de datos personales en posesión de sujetos
+        </a>
+        <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/LGMR_200521.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+        Ley General de Mejora Regulatoria 
+        </a>
+
+        <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/LGPC.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+        Ley General De Protección Civil
+        </a>
+        <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/LGCG.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+        Ley General de Contabilidad Gubernamental
+        </a>
+
+        <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/LGSNA_200521.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+        Ley General del Sistema Nacional Anticorrupción
+      </a>
+      <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/LGRA.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley General de Responsabilidades Administrativas
+      </a>
+
+      <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/LGMDFP.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley General en materia de desaparición forzada de Personas, desaparición cometida por particulares y del Sistema Nacional de Búsqueda de personas      </a>
+
+      <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/LAmp.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Amparo, Reglamentaria de los Articulos 103 y 107 de la Constitución Politica de los Estados Unidos Mexicanos      
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LFRCF_200521.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Fiscalización y Rendición de Cuentas de la Federacion
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/56_200521.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Obras Públicas y Servicios Relacionados con las Mismas 
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LFPRH.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Federal de Presupuesto y Responsabilidad Hacendaria
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/260.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley del Servicio Profesional de Carrera en la Administración Pública Federal 
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LFPDPPP.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Federal de Protección de datos personales en posesión de los partículares
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LFRSP.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Federal de Responsabilidades de los Servidores Publicos 
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LFTAIP.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Federal de Transparencia y Acceso a la Informacion Pública
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LFT.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Federal del Trabajo 
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LFPIORPI_200521.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      	Ley Federal para la Prevención e Identificacion de Operaciones con Recurso de Procedencia Ilicita
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LFPIORPI_200521.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Federal para la Prevención e Identificacion de Operaciones con Recurso de Procedencia Ilicita
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LFRCF_200521.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Fiscalización y Rendición de Cuentas de la Federacion
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LDFEFM.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Disciplina Financiera de las Entidades Federativas y los Municipios
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LAgra.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Agraria
+      </a>
+      <a href="https://www.diputados.gob.mx/LeyesBiblio/pdf/105.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Federal de Correduría Pública 
+      </a>
+      <a href="	https://www.oas.org/es/cidh/mandato/documentos-basicos/convencion-americana-derechos-humanos.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Pacto de San José de Costa Rica
+      </a>
+      <a href="http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20Organica%20de%20la%20Administracion%20Publica%20para%20el%20Estado.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Orgánica de la Administración Pública para el Estado de Hidalgo 	
+      </a>
+      <a href="	https://www.diputados.gob.mx/LeyesBiblio/pdf/LOTFJA.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Orgánica del Tribunal Federal de Justicia Administrativa
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20Organica%20Municipal%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Orgánica Municipal para el Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Archivos%20para%20el%20Estado%20de%20Hidalgo181119.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Archivos para el Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Adquisiciones,%20Arrendamientos%20y%20Servicios%20del%20Sector%20Publico.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Adquisiciones, Arrendamientos y Servicios del Sector Público del Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Entidades%20Paraestatales%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Entidades Paraestatales del Estado de Hidalgo 
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Presupuesto%20y%20Contabilidad%20Gubernamental%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Presupuesto y Contabilidad Gubernamental del Estado de Hidalgo 
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Proteccion%20de%20Datos%20Personales%20en%20posesion%20de%20sujetos%20obligados.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Protección de Datos Personales en Posesión de Sujetos Obligados para el Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Responsabilidades%20Administrativas%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Responsabilidades Administrativas del Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Responsabilidades%20de%20los%20Servidores%20Publicos%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Responsabilidades de los Servidores Públicos para el Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Transparencia%20y%20Acceso%20a%20la%20Informacion%20Publica%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Transparencia y Acceso a la Información Pública para el Estado de Hidalgo
+      </a>
+      <a href="v	https://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Poblacion%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Población para el Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Proteccion%20Civil%20del%20Estado%20de%20Hgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Protección Civil del Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20del%20Periodico%20Oficial%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley del Periódico Oficial del Estado de Hidalgo 
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20Estatal%20del%20Procedimiento%20Administrativo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Estatal del Procedimiento Administrativo 
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20Organica%20del%20Instituto%20de%20Defensoria%20Publica%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley Orgánica del Instituto de la Defensoría Pública del Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Propiedad%20en%20Condominio%20de%20Inmuebles%20para%20el%20Estado.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Propiedad en Condominio de Inmuebles para el Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Coordinacion%20Fiscal%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Coordinación Fiscal para el Estado de Hidalgo 
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Bienes%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Bienes del Estado de Hidalgo 
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Catastro%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Catastro del Estado de Hidalgo 
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Derechos%20Humanos%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Derechos Humanos del Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Deuda%20Publica%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Deuda Pública del Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Hacienda%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Hacienda del Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Fiscalizacion%20Superior%20y%20Rendicion%20de%20Cuentas%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Fiscalización Superior y Rendición de cuentas del Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Justicia%20para%20Adolecentes%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Justicia para Adolescentes del Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Acceso%20de%20las%20Mujeres%20a%20una%20Vida%20Libre%20de%20Violencia.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Acceso de las Mujeres a una vida libre de violencia para el Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20los%20Derechos%20de%20Ninas,%20Ninos%20y%20Adolescentes%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de los Derechos de las Niñas, Niños y adolescentes para el Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Victimas%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Victimas para el Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20los%20Trabajadores%20al%20Serv%20de%20los%20Gobs%20Est%20y%20Mples%20asi%20como%20de%20los%20Org%20Des.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de los Trabajadores al Servicio de los Gobiernos Estatal y Municipales, así como de los Organismos Descentralizados del Estado de Hidalgo 
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Obras%20Publicas%20y%20Servicios%20relacionados%20con%20las%20mismas%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Obras Públicas y Servicios Relacionados con las Mismas para el Estado de Hidalgo 
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Planeacion%20y%20Prospectiva%20del%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Planeacion y Prospectiva del Estado de Hidalgo
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20de%20Poblacion%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley de Población para el Estado de Hidalgo
+      </a>
+      <a href="http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20del%20Instituto%20Hidalguense%20de%20las%20Mujeres.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley del Instituto Hidalguense de las Mujeres 
+      </a>
+      <a href="	http://www.congreso-hidalgo.gob.mx/biblioteca_legislativa/leyes_cintillo/Ley%20del%20Notariado%20para%20el%20Estado%20de%20Hidalgo.pdf" target="_blank" rel="noopener noreferrer" className="list-group-item">
+      Ley del Notariado para el Estado de Hidalgo
+      </a>
       </div>
     </div>
   );
